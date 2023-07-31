2d014c65faa298a8ffd4feb949ca3f9a9b0fc806
Balthazar Neveu <balthazarneveu@gmail.com>
2023-08-01 00:45:26 +0200

    Filter inherits from FilterCore

--- src/interactive_pipe/filter.py
+++ src/interactive_pipe/filter.py
@@ -1,23 +1,64 @@
 import copy
+from typing import List
 
 from cache import CachedResults
 from sliders import KeyboardSlider
 
 
-class Filter:
-    """
-    Image processing single block is defined by the `apply` function to process multiple images
-    """
-
-    def __init__(self, name, sliders=None, inputs=[0], outputs=[0], cache=True):
+class FilterCore:
+    def __init__(self, name, inputs: List[int] = [0], outputs: List[int] = [0], cache=True):
         self.name = name
         self.cache = cache
         self.inputs = inputs
         self.outputs = outputs
         self.global_params = {}
+        self.reset_cache()
+        pass
+
+    def apply(self, *imgs, **kwargs) -> list:
+        """
+        :param imgs: img0, img1, img2, value1 , value2 , value3 ....
+            - (img0 is the result from the previous step)
+            - indexes of images processed is defined by `self.inputs`
+            - indexes of output images to be processed are defined by `self.outputs`
+            - then follow the parameters to be applied  `self.values` depicted by `self.sliderslist`
+        :param kwargs: dictionary containing all parameters
+        :return: output1, output2 ...
+        """
+        raise NotImplementedError("Need to implement the apply method")
+
+    def get_default_sliders(self) -> dict:
+        """Useful to define default sliders"""
+        return {}
+
+    def set_global_params(self, global_params: dict):
+        self.global_params = global_params
+
+    def reset_cache(self):
+        if self.cache:
+            self.cache_mem = CachedResults(self.name)
+        else:
+            self.cache_mem = None
+
+    def __repr__(self) -> str:
+        descr = "%s\n" % self.name
+        if not (self.inputs == [0] and self.outputs == [0]):
+            descr += "(" + (",".join(["%d" % it for it in self.inputs])) + ")"
+            descr += "->" + \
+                "(" + ",".join(["%d" % it for it in self.outputs]) + ")\n"
+        descr += "\n"
+        return descr
+
+
+class Filter(FilterCore):
+    """
+    Image processing single block is defined by the `apply` function to process multiple images
+    """
+
+    def __init__(self, name, sliders: dict = None, inputs: List[int] = [0], outputs: List[int] = [0], cache=True):
+        super().__init__(name, inputs=inputs, outputs=outputs, cache=cache)
         self.cursor = None
         self.cursor_cbk = None
-        self.reset_cache()
         if sliders is None:
             sliders = self.get_default_sliders()
         if sliders is not None:
@@ -50,37 +91,12 @@ class Filter:
         """Useful to define default sliders"""
         return {}
 
-    def __repr__(self):
-        descr = "%s\n" % self.name
-        if not (self.inputs == [0] and self.outputs == [0]):
-            descr += "(" + (",".join(["%d" % it for it in self.inputs])) + ")"
-            descr += "->" + \
-                "(" + ",".join(["%d" % it for it in self.outputs]) + ")\n"
+    def set_cursor(self, cursor):
+        self.cursor = cursor
+
+    def __repr__(self) -> str:
+        descr = super().__repr__()[:-1]
         for idx, sname in enumerate(self.sliderslist):
             descr += "\t%s=%.3f" % (sname, self.values[idx])
         descr += "\n"
         return descr
-
-    def apply(self, *imgs, **kwargs) -> list:
-        """
-        :param imgs: img0, img1, img2, value1 , value2 , value3 ....
-            - (img0 is the result from the previous step)
-            - indexes of images processed is defined by `self.inputs`
-            - indexes of output images to be processed are defined by `self.outputs`
-            - then follow the parameters to be applied  `self.values` depicted by `self.sliderslist`
-        :param kwargs: dictionary containing all parameters
-        :return: output1, output2 ...
-        """
-        raise NotImplementedError("Need to implement the apply method")
-
-    def set_global_params(self, global_params: dict):
-        self.global_params = global_params
-
-    def set_cursor(self, cursor):
-        self.cursor = cursor
-
-    def reset_cache(self):
-        if self.cache:
-            self.cache_mem = CachedResults(self.name)
-        else:
-            self.cache_mem = None
