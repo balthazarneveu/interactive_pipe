b67e6d98e142bb438d96a6b6309333c95e9d4f94
Balthazar Neveu <balthazarneveu@gmail.com>
2023-08-02 16:08:53 +0200

    PipelineCore is functional and partially tested

--- src/interactive_pipe/core/pipeline.py
+++ src/interactive_pipe/core/pipeline.py
@@ -6,42 +6,56 @@ class PipelineCore:
     """A pipeline is defined as the combination of:
     - a list of filters
     - an engine to execute the filters (with cache or not)
-    - optinally, some inputs to process
+    - optionally, some inputs to process
     """
 
-    def __init__(self, filters: List[FilterCore], name="pipeline", cache=False, inputs: Optional[list] = None, parameters: dict = {}):
+    def __init__(self, filters: List[FilterCore], name="pipeline", cache=False, inputs: Optional[list] = None, parameters: dict = {}, global_params={}):
         if not all(isinstance(f, FilterCore) for f in filters):
             raise ValueError(
                 "All elements in 'filters' must be instances of 'Filter'.")
         self.filters = filters
         self.engine = PipelineEngine(cache, safe_input_buffer_deepcopy=True)
-        self.parameters = parameters
         for filter in self.filters:
-            filter.set_global_params(self.parameters)
+            # link each global params
+            filter.set_global_params(global_params)
             filter.reset_cache()
         self.inputs = inputs
-        # You need to set the values to their default_value
-        if parameters is not None:
-            self.set_parameters(parameters)
+        # You need to set the values to their default_value for each filter
+        self.parameters = parameters
 
     def run(self) -> list:
         """Useful for standalone python acess without gui or disk write
         """
         return self.engine.run(self.filters, self.inputs)
-
-    def set_parameters(self, parameters: Dict[str, any]):
+    
+    @property
+    def parameters(self):
+        parameters = {}
+        for filt in self.filters:
+            parameters[filt.name] = parameters.values
+        return parameters
+    
+    @parameters.setter
+    def parameters(self, new_parameters: Dict[str, any]):
         """Force tuning parameters
+
+        ```
+        new_parameters = {
+            'filter1' : {'param1_1': 5, 'param1_2': 10},
+            'filter2' : {'param2_1':8}
+        }
+        ```
+        filter1.values = new_parameters["filter1"]
+        filter2.values = new_parameters["filter2"]
+
+        scan all new_parameters, check if the key describe the name of an avaiable filter,
+        then update that given filter parameters.
+
+        assume filter2 needed a second parameter `param2_2` which was defined when instantiating the filter...
+        `param2_2` will stay untouched
         """
-        for pa in self.filters:
-            if pa.name in parameters.keys():
-                # directly set from the dictionary
-                pa.values = parameters[pa.name]
-            else:
-                # FIXME: there's a very ugly mapping between slider_values (a list) & values (dictionary)
-                for idx, _pa_name in enumerate(pa.sliderslist):
-                    pa.values[list(pa.values.keys())[idx]
-                              ] = pa.defaultvalue[idx]
-        self.parameters = parameters
-        # for each slider, transmit global parameters
-        for slider in self.filters:
-            slider.set_global_params(parameters)
+        available_filters_names = [filt.name for filt in self.filters]
+        for filter_name in new_parameters.keys():
+            assert filter_name in available_filters_names, f"filter {filter_name} does not exist {available_filters_names}"
+            self.filters[available_filters_names.index(filter_name)].values = new_parameters[filter_name]
+    
--- test/test_engine.py
+++ test/test_engine.py
@@ -4,11 +4,13 @@ from copy import deepcopy
 
 try:
     from interactive_pipe.core.filter import FilterCore
-    from interactive_pipe.core.filter import PipelineEngine
+    from interactive_pipe.core.engine import PipelineEngine
+    from interactive_pipe.core.pipeline import PipelineCore
 except:
     import helper
     from core.filter import FilterCore
     from core.engine import PipelineEngine
+    from core.pipeline import PipelineCore
 
 input_image = np.array([[1, 2, 3], [4, 5, 6]])
 
@@ -22,22 +24,25 @@ def blend(img1, img2, blend_coeff=0.4):
     blended = blend_coeff*img1+(1-blend_coeff)*img2
     return [blended]
 
-# NOTE: tests both FilterCore & PipelineEngine
+# NOTE: tests both FilterCore & PipelineEngine & PipelineCore
 
 
 @pytest.mark.parametrize("cache", [True, False])
-def test_engine(cache):
+@pytest.mark.parametrize("engine_flag", [True, False])
+def test_engine(cache, engine_flag):
     # Define two filters:
     # - first filter  =  mad (multiply and add)
     # - second filter =  blending between 2 images
     # Default parameters are used.
     filt1 = FilterCore(apply_fn=mad, outputs=[2])
     filt2 = FilterCore(apply_fn=blend, inputs=[0, 2], outputs=[8])
-    cache = True
-    pip = PipelineEngine(cache=cache, safe_input_buffer_deepcopy=True)
+    if engine_flag:
+        pip = PipelineEngine(cache=cache, safe_input_buffer_deepcopy=True)
+    else:
+        pip = PipelineCore(filters=[filt1, filt2], cache=cache, inputs=[input_image])
     expected_filt1 = (2*input_image - 3)
     # 1/ execute for the first time
-    res = pip.run([filt1, filt2], imglst=[input_image])
+    res = pip.run([filt1, filt2], imglst=[input_image]) if engine_flag else pip.run()
     if cache:
         assert filt1.cache_mem.state_change.update_needed
         assert filt2.cache_mem.state_change.update_needed
@@ -47,15 +52,18 @@ def test_engine(cache):
 
     # 2/ do it again
     # cache shall be used for the two filters as no parameters were updated
-    res = pip.run([filt1, filt2], imglst=[input_image])
+    res = pip.run([filt1, filt2], imglst=[input_image]) if engine_flag else pip.run()
     if cache:
         assert not filt1.cache_mem.state_change.update_needed
         assert not filt2.cache_mem.state_change.update_needed
 
     # 3/ change parameters of the second filter and go again
     # first filter shall use its cache, second filter needs reprocessing
-    filt2.values["blend_coeff"] = 0.8
-    res = pip.run([filt1, filt2], imglst=[input_image])
+    if engine_flag:
+        filt2.values["blend_coeff"] = 0.8
+    else:
+        pip.parameters = {"blend": {"blend_coeff": 0.8}}
+    res = pip.run([filt1, filt2], imglst=[input_image]) if engine_flag else pip.run()
     assert np.allclose(res[8], (0.8*input_image+0.2*expected_filt1))
 
     if cache:
@@ -63,7 +71,7 @@ def test_engine(cache):
         assert filt2.cache_mem.state_change.update_needed
 
     # 4/ do it once more, nothing has changed, cache is reused
-    res = pip.run([filt1, filt2], imglst=[input_image])
+    res = pip.run([filt1, filt2], imglst=[input_image]) if engine_flag else pip.run()
     assert np.allclose(res[8], (0.8*input_image+0.2*expected_filt1))
     if cache:
         assert not filt1.cache_mem.state_change.update_needed
