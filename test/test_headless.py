cbf77caae7e019c3ad952eb55191a54f445ade4b
Balthazar Neveu <balthazarneveu@gmail.com>
2023-08-03 22:46:00 +0200

    update pytest, clean up folder after test

--- test/test_headless.py
+++ test/test_headless.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 from copy import deepcopy
+import shutil
 
 try:
     from interactive_pipe.core.filter import FilterCore
@@ -39,12 +40,15 @@ def get_sample_image():
     img[:200, :, :] *= 0.5
     return img
 
-def test_headless_pipeline():
+def test_headless_pipeline(tmp_path_factory):
     input_image = get_sample_image()
     filt1 = FilterCore(apply_fn=mad, name="mad", outputs=[1])
     filt2 = FilterCore(apply_fn=blend, inputs=[0, 1], outputs=[6])
     pip = HeadlessPipeline(filters=[filt1, filt2], inputs=[input_image], global_params={"ratio": 5})
-    pip.save("_image.png", data_wrapper_fn=lambda x: Image(x))
+    out_path = tmp_path_factory.mktemp("data_1")
+    assert out_path.exists()
+    pip.save(out_path/"_image.jpg", data_wrapper_fn=lambda x: Image(x))
+    shutil.rmtree(out_path) # clean  temporary folder
 
 
 def test_headless_pipeline_outputs_images(tmp_path_factory):
@@ -52,8 +56,10 @@ def test_headless_pipeline_outputs_images(tmp_path_factory):
     filt1 = FilterCore(apply_fn=mad, name="mad", outputs=[1])
     filt2 = FilterCore(apply_fn=blend_image_out, inputs=[0, 1], outputs=[6])
     pip = HeadlessPipeline(filters=[filt1, filt2], inputs=[input_image], global_params={"ratio": 5})
-    out_path = tmp_path_factory.mktemp("data")
+    out_path = tmp_path_factory.mktemp("data_2")
     assert out_path.exists()
     pip.save(out_path/"_image_from_Image_class.png")
     pip.export_tuning(out_path/"tuning/tuning.yaml")
     pip.import_tuning(out_path/"tuning/tuning.yaml")
+    shutil.rmtree(out_path) # clean  temporary folder
+
