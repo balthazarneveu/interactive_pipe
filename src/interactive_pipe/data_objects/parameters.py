f4a67379d8345e233611b487f9bac0f18c4b56b1
Balthazar Neveu <balthazarneveu@gmail.com>
2023-08-03 23:38:58 +0200

    parameters support yaml & json. pytests added

--- src/interactive_pipe/data_objects/parameters.py
+++ src/interactive_pipe/data_objects/parameters.py
@@ -1,15 +1,26 @@
 from pathlib import Path
 import yaml
+import json
 from yaml.loader import SafeLoader
 from data_objects.data import Data
 
 class Parameters(Data):
     def _set_file_extensions(self):
-        self.file_extensions = '.yaml'
+        self.file_extensions = ['.yaml', '.json']
     def _load(self, path: Path):
-        with open(path) as yaml_file:
-            params = yaml.load(yaml_file, Loader=SafeLoader)
+        with open(path) as file:
+            if path.suffix == '.yaml':
+                params = yaml.load(file, Loader=SafeLoader)
+            elif path.suffix == '.json':
+                params = json.load(file)
+            else:
+                raise ValueError(f"Unsupported file extension: {path.suffix}")
         return params
     def _save(self, path:Path):
         with open(path, 'w') as outfile:
-            yaml.dump(self.data, outfile, default_flow_style=False)
+            if path.suffix == '.yaml':
+                yaml.dump(self.data, outfile, default_flow_style=False)
+            elif path.suffix == '.json':
+                json.dump(self.data, outfile)
+            else:
+                raise ValueError(f"Unsupported file extension: {path.suffix}")
---
+++ test/test_parameters.py
@@ -0,0 +1,33 @@
+import pytest
+from pathlib import Path
+import shutil
+
+try:
+    from interactive_pipe.data_objects.parameters import Parameters
+except:
+    import helper
+    from data_objects.parameters import Parameters
+
+SAMPLE_DICT = {"key1": 25, "key2": {"key_2_1": 1, "key_2_2": True}, "key_3": [True, 1.256664, False]}
+
+@pytest.mark.parametrize("extension", ["json", "yaml"])
+def test_save(tmp_path, extension):
+    params = Parameters(SAMPLE_DICT)
+    file_path = tmp_path / f"test.{extension}"
+    params.save(str(file_path))
+    assert file_path.is_file()
+    params_reloaded = Parameters(file_path)
+    assert params_reloaded.data == SAMPLE_DICT
+    assert Parameters.load_from_file(file_path) == SAMPLE_DICT
+    assert Parameters.from_file(file_path).data == SAMPLE_DICT
+
+
+def test_invalid_file_extension(tmp_path):
+    file_path = tmp_path/"fake.txt"
+    if file_path.exists():
+        file_path.unlink()
+    with pytest.raises(AssertionError):
+        params = Parameters.from_file(file_path)
+    file_path.write_text("fake")
+    with pytest.raises(AssertionError):
+        params = Parameters.from_file(file_path)
