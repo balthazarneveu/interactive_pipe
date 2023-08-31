d3f91cb8c130b5745d3f3ec7f2c6b16d747348c4
Balthazar Neveu <balthazarneveu@gmail.com>
2023-09-01 01:44:01 +0200

    #4 keyboard controls matplotlib

--- src/interactive_pipe/graphical/mpl_gui.py
+++ src/interactive_pipe/graphical/mpl_gui.py
@@ -5,8 +5,18 @@ from typing import List
 import  logging
 from interactive_pipe.graphical.mpl_control import ControlFactory
 from interactive_pipe.graphical.mpl_window import MatplotlibWindow
+from interactive_pipe.data_objects.image import Image
+from interactive_pipe.data_objects.parameters import Parameters
 
 class InteractivePipeMatplotlib(InteractivePipeGUI):
+    """Interactive image pipe. Use sliders to fine tune your parameters
+    W to write full resolution image to disk
+    R to reset parameters
+    I to print parameters dictionary in the command line
+    E to export parameters dictionary to a yaml file
+    O to import parameters dictionary from a yaml file (sliders will not update)
+    H show help
+    """
     def init_app(self, fullscreen=False, **kwargs):
         self.fullscreen = fullscreen
         self.window = MainWindow(controls=self.controls, name=self.name, pipeline=self.pipeline, **kwargs)
@@ -22,9 +32,46 @@ class InteractivePipeMatplotlib(InteractivePipeGUI):
             except Exception as exc:
                 print(exc)
                 logging.warning("Cannot maximize screen")
-
+        self.window.fig.canvas.mpl_disconnect(
+                self.window.fig.canvas.manager.key_press_handler_id)
+        self.window.fig.canvas.mpl_connect('key_press_event', self.press)
         plt.show()
 
+    def press(self, event):
+        if False:
+            pass
+        # elif event.key == 'r':
+        #     self.resetsliders(forcereset=True, addslider=False)
+        elif event.key == 's' or event.key == 'w':
+            pth = Image.check_path(Image.prompt_file(), load=False)
+            self.pipeline.save(pth, data_wrapper_fn=lambda im:Image(im), save_entire_buffer=True)
+        elif event.key == 'o':
+            pth = Parameters.check_path(Parameters.prompt_file())
+            self.pipeline.import_tuning(pth)
+            self.window.pipeline = self.pipeline
+            self.window.need_redraw = True
+            # Requires mapping the parameters back into each Control objects
+            print("------------")
+            # @TODO: issue #18 - https://github.com/balthazarneveu/interactive_pipe/issues/18
+            for widget_idx, widget in self.window.ctrl.items():
+                matched = False
+                for filtname, params in self.pipeline.parameters.items():
+                    for param_name in params.keys():
+                        if param_name == widget.parameter_name_to_connect:
+                            print(f"MATCH & update {filtname} {widget_idx} with {self.pipeline.parameters[filtname][param_name]}")
+                            self.window.ctrl[widget_idx].update(self.pipeline.parameters[filtname][param_name])
+                            matched = True
+                assert matched, f"could not match widget {widget_idx} with parameter to connect {widget.parameter_name_to_connect}"
+            print("------------")
+            self.window.refresh()
+        elif event.key == 'e':
+            self.pipeline.export_tuning()
+        elif event.key == 'h':
+            print(self.__doc__)
+        elif event.key == 'i':
+            print(self.pipeline.__repr__())
+        elif event.key == 'q':
+            plt.close(self.window.fig)
 
 
 class MainWindow(MatplotlibWindow):
