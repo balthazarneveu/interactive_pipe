e01249e11570c1c3d35decd0c94cda6bc6779467
Balthazar Neveu <balthazarneveu@gmail.com>
2023-09-04 17:45:17 +0200

    #5 update pytests: check no inputs, single & multi inputs in a pipeline

--- test/test_headless.py
+++ test/test_headless.py
@@ -1,5 +1,6 @@
 import shutil
 import pytest
+import numpy as np
 from sample_functions import get_sample_image
 from interactive_pipe.core.filter import FilterCore
 from interactive_pipe.headless.pipeline import HeadlessPipeline
@@ -17,6 +18,9 @@ def blend_image_out(img1, img2, blend_coeff=0.4):
     blended = blend_coeff*img1+(1-blend_coeff)*img2
     return [Image(blended, title=f"blend={blend_coeff:0.2f}")]
 
+
+def generate_uniform(gray_level=0.5):
+    return gray_level*np.ones((5, 5, 3))
 
 def test_headless_pipeline(tmp_path_factory):
     input_image = get_sample_image()
@@ -46,7 +50,7 @@ def test_headless_pipeline_outputs_images(tmp_path_factory):
 
 
 @pytest.mark.parametrize("routing_indexes", [True, False])
-def test_headless_pipeline_single_input(tmp_path_factory, routing_indexes):
+def test_headless_pipeline_single_input(routing_indexes):
     input_image = get_sample_image()
     if routing_indexes:
         IMG_IN = 0
@@ -85,3 +89,66 @@ def test_headless_pipeline_single_input(tmp_path_factory, routing_indexes):
     with pytest.raises(AssertionError):
         pip(inputs={"toto": input_image})
     pip(inputs={IMG_IN: input_image})
+
+
+
+
+
+@pytest.mark.parametrize("routing_indexes", [True, False])
+def test_headless_pipeline_multi_input(routing_indexes):
+    input_image = get_sample_image()
+    if routing_indexes:
+        IMG_IN_1 = 0
+        IMG_IN_2 = 2
+        MAD_OUT = 4
+        BLEND_OUT = 8
+    else:
+        IMG_IN_1 = "image_1"
+        IMG_IN_2 = "image_2"
+        MAD_OUT= "exposed"
+        BLEND_OUT = "blended"
+    filt1 = FilterCore(apply_fn=blend, inputs=[IMG_IN_1, IMG_IN_2], outputs=[BLEND_OUT])
+    filt2 = FilterCore(apply_fn=mad, name="mad", inputs=[IMG_IN_1], outputs=[MAD_OUT])
+    with pytest.raises(AssertionError):
+        # no inputs routing provided
+        pip = HeadlessPipeline(filters=[filt1, filt2], inputs=None, outputs=[BLEND_OUT, MAD_OUT], cache=True)
+        out = pip.run()
+        
+    
+    with pytest.raises(AssertionError):
+        # no inputs routing provided, still try to execute with two images
+        pip = HeadlessPipeline(filters=[filt1, filt2], inputs=None, outputs=[BLEND_OUT, MAD_OUT], cache=True)
+        out = pip(input_image, 0.5*input_image)
+
+    pip = HeadlessPipeline(filters=[filt1, filt2], inputs=[IMG_IN_1, IMG_IN_2], outputs=[BLEND_OUT, MAD_OUT], cache=True)
+    pip.inputs = input_image, 0.5*input_image
+    out = pip.run()
+    assert len(out) == 2
+    pip(inputs={IMG_IN_2: 0.3*input_image , IMG_IN_1: input_image})
+    with pytest.raises(AssertionError):
+        pip(inputs={IMG_IN_2: input_image})
+
+
+def test_headless_pipeline_no_input():
+    input_image = get_sample_image()
+    FLAT = "flat"
+    MAD_OUT= "exposed"
+    BLEND_OUT = "blended"
+    filt1 = FilterCore(apply_fn=generate_uniform, inputs=None, outputs=[FLAT])
+    filt2 = FilterCore(apply_fn=mad, name="mad", inputs=[FLAT], outputs=[MAD_OUT])
+    filt3 = FilterCore(apply_fn=blend, inputs=[FLAT, MAD_OUT], outputs=[BLEND_OUT])
+
+    pip = HeadlessPipeline(filters=[filt1, filt2, filt3], inputs=None, outputs=[FLAT, BLEND_OUT, MAD_OUT], cache=True)
+    pip()
+
+    pip = HeadlessPipeline(filters=[filt1, filt2, filt3], inputs=[], outputs=[FLAT, BLEND_OUT, MAD_OUT], cache=True)
+    pip()
+
+    pip = HeadlessPipeline(filters=[filt1, filt2, filt3], inputs=[], outputs=[FLAT, BLEND_OUT, MAD_OUT], cache=True)
+    pip.inputs = None
+    out = pip.run()
+
+    pip.inputs = []
+    out = pip.run()
+    assert len(out) == 3
+    
