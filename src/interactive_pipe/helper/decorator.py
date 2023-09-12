c60366d891cea03bd4abbc6685440a0792f80464
Balthazar Neveu <balthazarneveu@gmail.com>
2023-09-12 20:51:37 +0200

    forbid the declaration of multiple sliders using the same name

--- src/interactive_pipe/helper/decorator.py
+++ src/interactive_pipe/helper/decorator.py
@@ -8,6 +8,7 @@ import inspect
 from typing import Callable,Union
 import logging
 
+__registered_controls_names = []
 def interactive(**decorator_controls):
     """Function decorator to add some controls
     """
@@ -15,31 +16,57 @@ def interactive(**decorator_controls):
         controls = {}
         keyword_args = analyze_apply_fn_signature(func)[1]
         keyword_names =  list(keyword_args.keys())
+        global __registered_controls_names
+        
+        # Analyze at 2 levels (function keyword args & decorator keyword args)  then register controls when necessary.
+        #-------------------------------------------
+        # @interactive(param_2=Control(...), )
+        # def func(img1, img2, param_1=Control(...)):
+        #-------------------------------------------
+        
+        # 1. Analyzing function keyword args 
+        # def func(img1, img2, param_1=Control(...))
+        
         for param_name, unknown_keyword_arg in keyword_args.items():
-            if isinstance(unknown_keyword_arg, Control):
+            chosen_control = None
+            if isinstance(unknown_keyword_arg, Control): # This includes KeyboardControl aswell!!
                 if unknown_keyword_arg.name is None or unknown_keyword_arg._auto_named:
                     unknown_keyword_arg.name = param_name
-                controls[param_name] = unknown_keyword_arg
+                chosen_control = unknown_keyword_arg
             else:
                 if isinstance(unknown_keyword_arg, list) or isinstance(unknown_keyword_arg, tuple):
                     try:
-                        controls[param_name] = control_from_tuple(unknown_keyword_arg, param_name=param_name)
+                        chosen_control = control_from_tuple(unknown_keyword_arg, param_name=param_name)
                     except Exception as exc_1:
                         try:
-                            controls[param_name] = control_from_tuple((unknown_keyword_arg,), param_name=param_name)
+                            chosen_control = control_from_tuple((unknown_keyword_arg,), param_name=param_name)
                         except Exception as exc:
-                            print(exc_1)
                             raise Exception(exc)
                     # NOTE: for keyword args, setting a boolean will not trigger a tickmark (although it is possible)
                     # Use (True) instead of True if you want to make a tickbox
-
-
+            if chosen_control is None:
+                continue
+            assert chosen_control.name not in __registered_controls_names, f"{chosen_control.name} already attributed - {__registered_controls_names}"
+            __registered_controls_names.append(chosen_control.name)
+            controls[param_name] = chosen_control
+           
+        
+        # 2. Analyzing decorator keyword args 
+        # @interactive(param_2=Control(...))
         for param_name, unknown_control in decorator_controls.items():
+            chosen_control = None
             assert param_name in keyword_names, f"typo: control {param_name} passed through the decorator does not match any of the function keyword args {keyword_names}"
             if isinstance(unknown_control, Control):
-                controls[param_name] = unknown_control
+                if unknown_control.name is None or unknown_control._auto_named:
+                    unknown_control.name = param_name
+                chosen_control = unknown_control
             else: # you may get a tuple, list or a boolean
-                controls[param_name] = control_from_tuple(unknown_control, param_name=param_name)
+                chosen_control = control_from_tuple(unknown_control, param_name=param_name)
+            if chosen_control is None:
+                continue
+            assert chosen_control.name not in __registered_controls_names, f"{chosen_control.name} already attributed - {__registered_controls_names}"
+            __registered_controls_names.append(chosen_control.name)
+            controls[param_name] = chosen_control
 
         for param_name, unknown_control in controls.items():
             Control.register(func.__name__, param_name, controls[param_name])
