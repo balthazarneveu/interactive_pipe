5583e0bc543120730d669a8568bec8ef002a151d
Balthazar Neveu <balthazarneveu@gmail.com>
2023-09-15 13:28:46 +0200

    move pipeline decorator to dedicated file

--- src/interactive_pipe/__init__.py
+++ src/interactive_pipe/__init__.py
@@ -1,4 +1,4 @@
-from interactive_pipe.helper.decorator import pipeline, interactive_pipeline
+from interactive_pipe.helper.pipeline_decorator import pipeline, interactive_pipeline
 from interactive_pipe.headless.control import Control
 from interactive_pipe.headless.keyboard import  KeyboardControl
 from interactive_pipe.helper.filter_decorator import interactive, interact
