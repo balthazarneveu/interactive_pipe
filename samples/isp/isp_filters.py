5fba13e23547f12761374352903b513e96b45a7e
Balthazar Neveu <balthazarneveu@gmail.com>
2023-09-29 09:50:04 +0200

    add histogram equalization in torch

--- samples/isp/isp_filters.py
+++ samples/isp/isp_filters.py
@@ -3,8 +3,8 @@ import numpy as np
 import torch
 from interactive_pipe import interactive_pipeline, interactive
 from isp_library import normalize_image, demosaick, apply_color_transform
-from isp_helper_filters import load_raw_buffer, visualize_tensor, bayer_crop, array_to_tensor, rawpi_prostprocess
-
+from isp_helper_filters import load_raw_buffer, visualize_tensor, bayer_crop, array_to_tensor, rawpi_prostprocess, histogram, Interp
+from interactive_pipe.data_objects.curves import Curve, SingleCurve
 
 
 # ----------------------------------------------------------------------------
@@ -44,7 +44,10 @@ def demosaick_filter(planar_bayer: torch.FloatTensor,
     return demosaick(planar_bayer, str(global_params["raw_metadata"]["raw_phase"]))
 
 # ----------------------------------------------------------------------------
-@interactive(apply_white_balance=(True, "white balance"), apply_color_matrix=(False, "color matrix"))
+@interactive(
+    apply_white_balance= (True, "white balance"),
+    apply_color_matrix=(False, "color matrix")
+)
 def neutral_to_realistic(
         rgb_linear: torch.FloatTensor,
         apply_white_balance=True,
@@ -78,28 +81,81 @@ def neutral_to_realistic(
 # ----------------------------------------------------------------------------
 @interactive(gamma=(2.2, [1., 2.2]))
 def apply_tone_curve(rgb: torch.FloatTensor, gamma: float=2.2) -> torch.FloatTensor:
-        """Gamma curve"""
-        return torch.pow(rgb, 1./gamma)
+    """Gamma curve"""
+    return torch.pow(rgb, 1./gamma)
+
+@interactive(bins=(512, [16, 4096, 64]))
+def compute_histogram(rgb, bins=512):
+    style="-"
+    name="histo"
+    luma = torch.mean(rgb, dim=(-3))
+    # histo, histo_bins = torch.histogram(luma, bins=256, range=(0,1), density=True) # Not working on a GPU
+    histo, histo_bins = histogram(luma, bins=bins, range=(0,1), density=True)
+    return (histo, histo_bins, style, name),
+
+
+def get_cumulative_histo(histo_in):
+    histo, histo_bins = histo_in[:2]
+    histo_cum = torch.cumsum(histo, 0)
+    return (histo_cum, histo_bins, "", "cum"),
+
+
+def get_equalization_tone_curve(histo_in, image):
+    histo, histo_bins = histo_in[:2]
+    histo_cum = torch.cumsum(histo, 0)
+    
+    interp = Interp([histo_bins[:-1]], histo_cum)
+    tone_mapped = interp([image.view(image.shape[0]*image.shape[-2]*image.shape[-1])])
+    tone_mapped = tone_mapped.view(image.shape)
+    return tone_mapped
+
+# ----------------------------------------------------------------------------
+def stack_histograms(*histos) -> torch.FloatTensor:
+    """Stack several histograms into a curve
+    No custom legend so far!
+    """
+    histo_curve = Curve(
+        [
+            (histo_bins[:-1].detach().to("cpu").numpy(), histo.detach().to("cpu").numpy(), "-", f"{name} {index}")
+            for index, (histo, histo_bins, style, name) in enumerate(histos)
+        ],
+        grid=True,
+        title="Histogram",
+        xlim=(0., 1.),
+        ylim=(0., 0.01)
+    )
+    return histo_curve
+
 
 # ----------------------------------------------------------------------------
 # ----------------------------------------------------------------------------
 def isp_pipeline(path: Path):
     bayer = load_raw_buffer(path)
-
     planar_bayer_tensor_full = array_to_tensor(bayer)
     planar_bayer_tensor_crop = bayer_crop(planar_bayer_tensor_full)
     planar_bayer_tensor = normalize_raw(planar_bayer_tensor_crop)
     bayer_tensor = planar_bayer_to_channels(planar_bayer_tensor)
     rgb_linear = demosaick_filter(bayer_tensor)
+    hist_raw = compute_histogram(rgb_linear) #, style="k--", name="raw")
     rgb = neutral_to_realistic(rgb_linear)
+    hist_lin = compute_histogram(rgb) #, style="g-", name="linear")
+    
     rgb = apply_tone_curve(rgb)
+    hist = compute_histogram(rgb) #, style="b-", name="rgb gamma")
+    tm = get_equalization_tone_curve(hist, rgb)
+    hist_eq = compute_histogram(tm) #, style="b-", name="rgb gamma")
+    
+    histogram_curve = stack_histograms(hist, hist_lin, hist_raw, hist_eq)
     rgb_viz = visualize_tensor(rgb)
-    return rgb_viz
+    tm_viz = visualize_tensor(tm)
+    
+    return rgb_viz, histogram_curve, tm_viz
 
 
 def isp_monolithic_rawpipipeline(path: Path):
     rgb_new = rawpi_prostprocess(path)
     rgb_new = bayer_crop(rgb_new)
+
     return rgb_new
 
 
--- samples/isp/isp_helper_filters.py
+++ samples/isp/isp_helper_filters.py
@@ -3,6 +3,7 @@ import rawpy
 from pathlib import Path
 import torch
 from interactive_pipe import interact, interactive
+from itertools import product
 import numpy as np
 import logging
 
@@ -95,7 +96,7 @@ def bayer_crop(planar_bayer, center_x=0.5, center_y=0.5, size=8.):
 
 # ---------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------
-# Move back tensors to CPU to visualize
+# Move tensors back to CPU to visualize
 def visualize_tensor(in_tensor: torch.FloatTensor, resize_flag=True, resize=256):
     tensor = in_tensor.clone()
     if len(tensor.shape) == 3:
@@ -114,3 +115,79 @@ def visualize_tensor(in_tensor: torch.FloatTensor, resize_flag=True, resize=256)
     tensor = tensor.contiguous()
     tensor = tensor.detach().to("cpu").numpy()
     return tensor
+
+
+
+# ---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
+# Histograms computation
+def histogram(xs, bins, range=None, density=True):
+    # https://github.com/pytorch/pytorch/issues/69519 
+    # Like torch.histogram, but works with cuda
+    if range is None:
+        mini_bin, maxi_bin = xs.min(), xs.max()
+    else:
+        mini_bin, maxi_bin = range
+    counts = torch.histc(xs, bins, min=mini_bin, max=maxi_bin)
+    boundaries = torch.linspace(mini_bin, maxi_bin, bins + 1, device=xs.device)
+    if density:
+        counts = counts/counts.sum()
+    return counts, boundaries
+
+
+
+# ---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
+class Interp:
+    # https://github.com/sbarratt/torch_interpolations/blob/master/torch_interpolations/multilinear.py
+    def __init__(self, points, values):
+        self.points = points
+        self.values = values
+
+        assert isinstance(self.points, tuple) or isinstance(self.points, list)
+        assert isinstance(self.values, torch.Tensor)
+
+        self.ms = list(self.values.shape)
+        self.n = len(self.points)
+
+        assert len(self.ms) == self.n
+
+        for i, p in enumerate(self.points):
+            assert isinstance(p, torch.Tensor)
+            assert p.shape[0] == self.values.shape[i]
+
+    def __call__(self, points_to_interp):
+        assert self.points is not None
+        assert self.values is not None
+
+        assert len(points_to_interp) == len(self.points)
+        K = points_to_interp[0].shape[0]
+        for x in points_to_interp:
+            assert x.shape[0] == K
+
+        idxs = []
+        dists = []
+        overalls = []
+        for p, x in zip(self.points, points_to_interp):
+            idx_right = torch.bucketize(x, p)
+            idx_right[idx_right >= p.shape[0]] = p.shape[0] - 1
+            idx_left = (idx_right - 1).clamp(0, p.shape[0] - 1)
+            dist_left = x - p[idx_left]
+            dist_right = p[idx_right] - x
+            dist_left[dist_left < 0] = 0.
+            dist_right[dist_right < 0] = 0.
+            both_zero = (dist_left == 0) & (dist_right == 0)
+            dist_left[both_zero] = dist_right[both_zero] = 1.
+
+            idxs.append((idx_left, idx_right))
+            dists.append((dist_left, dist_right))
+            overalls.append(dist_left + dist_right)
+
+        numerator = 0.
+        for indexer in product([0, 1], repeat=self.n):
+            as_s = [idx[onoff] for onoff, idx in zip(indexer, idxs)]
+            bs_s = [dist[1 - onoff] for onoff, dist in zip(indexer, dists)]
+            numerator += self.values[as_s] * \
+                torch.prod(torch.stack(bs_s), dim=0)
+        denominator = torch.prod(torch.stack(overalls), dim=0)
+        return numerator / denominator
