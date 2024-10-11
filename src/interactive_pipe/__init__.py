9fc54cb8c7781bc6aec0c3a346c5b70f1fbd4d02
Balthazar Neveu <balthazarneveu@gmail.com>
2024-10-11 13:58:01 +0200

    update to v0.7.9

--- src/interactive_pipe/__init__.py
+++ src/interactive_pipe/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.7.8"
+__version__ = "0.7.9"
 from interactive_pipe.helper.pipeline_decorator import pipeline, interactive_pipeline
 from interactive_pipe.headless.control import Control, CircularControl
 from interactive_pipe.data_objects.curves import Curve, SingleCurve
