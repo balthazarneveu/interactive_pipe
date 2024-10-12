17de4bb8f9e855b9e3f67f401b1f58f746d5882e
Balthazar Neveu <balthazarneveu@gmail.com>
2024-10-12 16:55:52 +0200

    update to v0.8.1 (gradio+audio fixes)

--- src/interactive_pipe/__init__.py
+++ src/interactive_pipe/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 from interactive_pipe.helper.pipeline_decorator import pipeline, interactive_pipeline
 from interactive_pipe.headless.control import Control, CircularControl
 from interactive_pipe.data_objects.curves import Curve, SingleCurve
